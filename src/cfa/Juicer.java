06f2c1290fb02b6be024979b25f131b0e716ff95
najat-el <najatelhabti1997@gmail.com>
2020-11-09 08:57:46 +0100

    Solution for "Juicer" problem

---
+++ src/cfa/Juicer.java
@@ -0,0 +1,31 @@
+package cfa;
+
+import java.util.Scanner;
+
+public class Juicer {
+    static public void main(String[] args){
+        Scanner scanner=new Scanner(System.in);
+        int n=scanner.nextInt();
+        int b=scanner.nextInt();
+        int d=scanner.nextInt();
+
+        int sum=0;
+        int emptyJuicerCount=0;
+        int tmp;
+
+        for (int i = 0; i <n ; i++) {
+            tmp=scanner.nextInt();
+
+            if(tmp<=b)
+                sum+=tmp;
+
+            if(sum> d){
+                emptyJuicerCount++;
+                sum=0;
+            }
+
+        }
+
+        System.out.println(emptyJuicerCount);
+    }
+}
