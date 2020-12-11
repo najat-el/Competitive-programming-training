a614bccd8afcf410ffe3ff3878d1aaf3cc5dd88b
najat-el <najatelhabti1997@gmail.com>
2020-12-11 10:02:15 +0100

    Solution for "New Password" problem

---
+++ src/cfa/NewPassword.java
@@ -0,0 +1,39 @@
+package cfa;
+
+import java.util.Scanner;
+
+/**
+ * Link to the problem : http://codeforces.com/contest/770/problem/A
+ *
+ * @author Najat EL Habti
+ */
+
+public class NewPassword {
+    public static void main(String[] args) {
+        Scanner scanner=new Scanner(System.in);
+
+        int n=scanner.nextInt();
+        int k=scanner.nextInt();
+
+        String password = "";
+        char[] letters=new char[k];
+        char c='a';
+
+        for (int i = 0; i <k ; i++) {
+            letters[i]=c;
+            c++;
+        }
+
+        int index=0;
+
+        while (password.length()<n){
+          if(index>=k)
+              index=0;
+
+          password+=letters[index];
+          index++;
+      }
+      System.out.println(password);
+      scanner.close();
+    }
+}
